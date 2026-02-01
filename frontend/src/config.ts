6b227d2f7f30e33814f4be75fab2c28418ae9c78
AlexanderTheWorst <alexander.backman@elev.ga.ntig.se>
2026-02-01 19:29:03 +0100

    Remove frontend folder to re-add as submodule

--- frontend/src/config.ts
+++
@@ -1,3 +0,0 @@
-export default  {
-    API_URI: "api.bahoot.local"
-}
--- frontend/src/routes/test/+page.tsx
+++
@@ -1,9 +0,0 @@
-// @ts-ignore
-import Icon from "../../assets/Icon.svg?react";
-import NavbarDefault from "@/components/Navbar/default.tsx";
-
-export default function() {
-    return <>
-        <NavbarDefault />
-    </>
-}
