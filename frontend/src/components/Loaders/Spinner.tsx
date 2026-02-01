058d0f41a9b2b923e1abac0332a293e448a32161
AlexanderTheWorst <alexander.backman@elev.ga.ntig.se>
2026-02-01 19:30:53 +0100

    sigma

--- frontend/src/components/Loaders/Spinner.tsx
+++
@@ -1,9 +0,0 @@
-import { type ComponentProps } from "preact/compat";
-
-export default function({ className, color }: ComponentProps<"div"> & { color?: string; }) {
-    return <div style={{
-        borderColor: color ?? "black",
-    }} className={(className ?? "") + " w-[48px] h-[48px] border-[4px] border-black border-b-transparent! rounded-full animate-spin"}>
-
-    </div>
-}
